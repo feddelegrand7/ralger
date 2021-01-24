129011715dd84a8b1ce5a62780a911dcf68e48a0
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-01-24 13:33:53 +0100

    adding the img_noalt_scrap() function

--- R/img_noalt_scrap.R
+++ R/img_noalt_scrap.R
@@ -1,5 +1,3 @@
-
-
 #' Scrape Images URLS that don't have 'alt' attributes
 #'
 #' @param link the URL of the web page
@@ -17,8 +15,7 @@
 #' @importFrom curl has_internet
 #' @importFrom utils download.file
 
-img_no_alt_scrap <- function(link, askRobot = FALSE) {
-
+img_noalt_scrap <- function(link, askRobot = FALSE) {
 
   if (missing(link)) {
 
@@ -31,76 +28,42 @@ img_no_alt_scrap <- function(link, askRobot = FALSE) {
     stop("the 'link' parameter must be provided
          as a character string")
 
-
   }
 
-  ###############
-  ####### Ask robot related ##################################################
+  ####### Ask robot related ################################################################
   if (askRobot) {
-
     if (paths_allowed(link) == TRUE) {
       message(green("the robot.txt doesn't prohibit scraping this web page"))
-
     }
     else {
       message(bgRed(
         "WARNING: the robot.txt doesn't allow scraping this web page"
       ))
-
-
     }
   }
   ##########################################################################################
-
   tryCatch(
-
     expr = {
-
       img_urls <- lapply(link, function(url) {
-
         url %>%
           read_html() %>%
           html_nodes("img:not([alt])")
-
       })
-
-      return(img_urls)
-
-
+      return(img_urls[[1]])
     },
 
     error = function(cond) {
-
       if (!has_internet()) {
-
         message(paste0("Please check your internet connexion: ", cond))
-
         return(NA)
-
       } else if (grepl("current working directory", cond) || grepl("HTTP error 404", cond)) {
-
         message(paste0("The URL doesn't seem to be a valid one: ", link))
-
         message(paste0("Here the original error message: ", cond))
-
         return(NA)
-
       } else {
-
         message(paste0("Undefined Error: ", cond))
-
         return(NA)
-
-
       }
     }
-
   )
-
-
-
-
-
-
-
 }
